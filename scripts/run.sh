35bdf5c44785125cb28d47d5c99001601cca6ac2
Yuhanun Citgez <y.citgez@student.utwente.nl>
2020-02-23 14:05:37 -0500

    Update script path

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Command to run container:  
-# docker run -v ~/opt/verona-bot:/opt/verona-bot yuhanuncitgez/verona-bot:latest "{num}"
+# docker run -v /opt/verona-bot:/opt/verona-bot yuhanuncitgez/verona-bot:latest "{num}"
 # Number = "$1"
 # Run "/opt/verona-bot/input/$1.verona"
 # And output into /opt/verona-bot/output/$1.verona
-/opt/verona/build_ninja/dist/veronac --run "/opt/verona-bot/input/$1.verona" > "/opt/verona-bot/output/$1.verona"
+sudo /opt/verona/build_ninja/dist/veronac --run "/opt/verona-bot/input/$1.verona" > "/opt/verona-bot/output/$1.verona"
