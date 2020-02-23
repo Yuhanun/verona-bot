50ed107bed13d9cc36c89255ed6dec1e965ce417
Yuhanun Citgez <y.citgez@student.utwente.nl>
2020-02-23 17:25:18 -0500

    Improved looks, that's all for the day i guess

--- cogs/verona.py
+++ cogs/verona.py
@@ -1,9 +1,12 @@
 """
 Verona compiler cog
 """
+import datetime
 
+import discord
 from discord.ext import commands
 from docker_runner import verona
+
 
 class Verona(commands.Cog, name="Verona"):
     """
@@ -34,8 +37,16 @@ class Verona(commands.Cog, name="Verona"):
         verona.set_code(num, code)
         success, result = await verona.run_container(num)
 
-        await ctx.send(f"Success? {success}")
-        await ctx.send(result)
+        if "error generated" in result:
+            success = False
+
+        embed = discord.Embed(title=f"Success: {success}", colour=discord.Colour(0xFF0000 if not success else 0x00FF00),
+                              description=f"Output:\n```\n{result}```", timestamp=datetime.datetime.utcnow())
+
+        embed.set_footer(text=str(ctx.message.author),
+                         icon_url=ctx.message.author.avatar_url)
+
+        await ctx.send(embed=embed)
 
 
 def setup(bot):
--- docker_runner/verona.py
+++ docker_runner/verona.py
@@ -5,10 +5,11 @@ import asyncio
 
 docker_client = docker.from_env()
 
+
 def get_num_and_inc() -> int:
     with open("/opt/verona-bot/counter.txt", "r") as file:
         counter = int(file.read().strip())
-    
+
     old = counter
     counter += 1
 
@@ -19,17 +20,20 @@ def get_num_and_inc() -> int:
 
     return old
 
+
 def set_code(num: int, code: str):
     with open(f"/opt/verona-bot/input/{num}.verona", "a") as file:
         file.write(code.strip())
 
+
 def get_output_path(num: int) -> str:
     return f"/opt/verona-bot/output/{num}.txt"
+
 
 async def run_container(num: int, timeout_count: int = 30) -> (bool, str, ):
     container = docker_client.containers.run("yuhanuncitgez/verona-bot:latest", f"/opt/verona-bot-scripts/run.sh \"{num}\"",
                                              stdout=True,
-                                             stderr=True, 
+                                             stderr=True,
                                              auto_remove=True,
                                              detach=True,
                                              volumes={
